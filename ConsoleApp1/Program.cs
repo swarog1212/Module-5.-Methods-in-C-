e9c344428aef0871e955b624a5a864d2dacad9c2
george <dark8609@gmail.com>
2021-10-11 12:18:31 +0300

    5.1 Методы

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            return color;
+        }
+
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+            int temp = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
+            foreach (var item in result)
+            {
+                Console.Write(item);
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var (name, age) = ("Георгий", 35);
+
+            Console.WriteLine("Мое имя: {0}", name);
+            Console.WriteLine("Мой возраст: {0}", age);
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите возрас с цифрами:");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+
+            string[] favcolor = new string[3];
+            for (int i = 0; i < favcolor.Length; i++)
+            {
+                favcolor[i] = ShowColor();
+            }
+            foreach (var item in favcolor)
+            {
+                Console.WriteLine(item);
+            }
+            int[] array = GetArrayFromConsole();
+            Console.ReadKey();
+            
+        }
+        
+    }
+}
