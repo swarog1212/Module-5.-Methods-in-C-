42fc7d7509a3efe8243f2525d4d87b878cd8cc25
george <dark8609@gmail.com>
2021-10-14 13:21:08 +0300

    5.5 рекарсивные функции. Факториал и возведение в степени

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -121,20 +121,37 @@ namespace ConsoleApp1
                 Console.WriteLine("Внимание сортировка невозможны!!");
             }
         }*/
-        static void Echo (string saidworld, int deep)
+        /*static void Echo (string saidworld, int deep)
         {
             var modif = saidworld;
             if (modif.Length > 2)
             {
                 modif = modif.Remove(0, 2);
             }
+            Console.BackgroundColor = (ConsoleColor)deep;
             Console.WriteLine("..." + modif);
             if (deep > 1)
             {
                 Echo(modif, deep - 1);
             }
+        }*/
+        static decimal Factorial (int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            return x * Factorial(x - 1);
         }
-
+        static int PowerUp (int N, byte pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            pow--;
+            return N * PowerUp(N, pow);
+        }
         static void Main(string[] args)
         {
             /*var (name, age) = ("Георгий", 35);
@@ -157,11 +174,15 @@ namespace ConsoleApp1
             int[] sorteddesc;
             int[] sortedasc;
             SortArray(array, out sorteddesc, out sortedasc);*/
-            Console.WriteLine("Напишите что-то");
-            var str = Console.ReadLine();
-            Console.WriteLine("Укажиет глубину эха");
-            int deep = int.Parse(Console.ReadLine());
-            Echo(str, deep);
+            //Console.WriteLine("Напишите что-то");
+            //var str = Console.ReadLine();
+            //Console.WriteLine("Укажиет глубину эха");
+            //int deep = int.Parse(Console.ReadLine());
+            //Echo(str, deep);
+            
+            Console.WriteLine(Factorial(20));
+            
+            Console.WriteLine(PowerUp(2, 3));
         }    
         
     }
